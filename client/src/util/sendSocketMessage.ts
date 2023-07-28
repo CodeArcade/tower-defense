a2ec66411a7ada0a2e640f2ab4334bb5d567d1a1
Cyklan <dev@cyklan.de>
2023-07-28 16:55:54 +0200

    extend function to error if correct key is not set

--- client/src/util/sendSocketMessage.ts
+++ client/src/util/sendSocketMessage.ts
@@ -2,11 +2,11 @@ import { Message } from "tower-defense-shared/dist/Messages";
 import { Socket } from "../App";
 import { Event } from "tower-defense-shared/dist/events";
 
-export const sendSocketMessage = <T extends Message>(
+export const sendSocketMessage = <T extends Message, M extends T["message"]>(
   socket: Socket,
   event: Event,
   messageType: T["type"],
-  message: T["message"]
+  message: M
 ) => {
   socket.send(event, {
     type: messageType,
